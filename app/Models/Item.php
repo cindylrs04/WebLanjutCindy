6048cb7d535d0b101a663df4839d4be615916831
cindylrs04 <larasaticindy231@gmail.com>
2025-02-17 15:39:02 +0700

    add komen pada item

--- app/Models/Item.php
+++ app/Models/Item.php
@@ -1,15 +1,17 @@
 <?php
 
 namespace App\Models;
+// Menentukan namespace model berada dalam folder App\Models
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
-use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory; // Menggunakan trait HasFactory untuk membuat factory model
+use Illuminate\Database\Eloquent\Model; // Menggunakan kelas Model sebagai dasar dari model Eloquent
 
-class Item extends Model
+// Mendefinisikan kelas Item sebagai model yang mewarisi Eloquent Model
+class Item extends Model 
 {
-    use HasFactory;
-    protected $fillable = [
-        'name',
-        'description',
+    use HasFactory; // Menggunakan trait HasFactory agar model ini dapat menggunakan fitur factory
+    protected $fillable = [ // Mendefinisikan atribut yang dapat diisi secara massal (mass assignment)
+        'name', // Mengizinkan atribut 'name' untuk diisi secara massal
+        'description', // Mengizinkan atribut 'description' untuk diisi secara massal
     ];
 }
