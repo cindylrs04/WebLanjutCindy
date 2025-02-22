4084d0b0ebb87bae28c292a65c0f8223ac606b78
cindylrs04 <larasaticindy231@gmail.com>
2025-02-22 14:05:09 +0700

    add soal praktikum

---
+++ POS/routes/web.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\ProductController;
+use App\Http\Controllers\UserController;
+use App\Http\Controllers\SalesController;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+// Route Home
+Route::get('/', [HomeController::class, 'index']);
+
+// Route Products (dengan prefix "category")
+Route::prefix('category')->group(function () {
+    Route::get('/food-beverage', [ProductController::class, 'foodBeverage']);
+    Route::get('/beauty-health', [ProductController::class, 'beautyHealth']);
+    Route::get('/home-care', [ProductController::class, 'homeCare']);
+    Route::get('/baby-kid', [ProductController::class, 'babyKid']);
+});
+
+// Route User (menggunakan parameter)
+Route::get('/user/{id}/name/{name}', [UserController::class, 'profile']);
+
+// Route Penjualan (POS)
+Route::get('/sales', [SalesController::class, 'index']);
