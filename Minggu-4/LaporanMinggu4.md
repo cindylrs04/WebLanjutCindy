57902f096325004a73c0a6236611d90eab461920
cindylrs04 <larasaticindy231@gmail.com>
2025-03-07 12:19:51 +0700

    add praktikum 1 jobsheet 4

---
+++ Minggu-4/LaporanMinggu4.md
@@ -0,0 +1,16 @@
+# Laporan Jobsheet Minggu ke-4 
+<b>Mata Kuliah : Pemrograman Web Lanjut</b></p>
+<b>Program Studi : D4 - Teknik Informatika</b></p>
+<b>Semester : 4</b>
+<hr>
+<b>Kelas : TI 2A</b></p>
+<b>NIM : 2341720038</b></p>
+<b>Nama : Cindy Laili Larasati</b>
+<hr>
+
+<b>Praktikum 1 - $fillable</b>
+<p>3.<p align="center">
+    <img src="Gambar/gambarP1No3.png"></p>
+<p>6. jika kolom di fillable di hapus maka akan seperti gambar di bawah
+<p align="center">
+    <img src="Gambar/gambarP1No6.png"></p>
--- PWL_POS/app/Http/Controllers/UserController.php
+++ PWL_POS/app/Http/Controllers/UserController.php
@@ -9,15 +9,16 @@ use Illuminate\Support\Facades\Hash;
 class UserController extends Controller
 {
     public function index(){
-    // tambah data user dengan Eloquent Model
         $data = [
-            'nama' => 'Pelanggan Pertama',
+            'level_id' => 2,
+            'username' => 'manager_tiga',
+            'nama' => 'manager 3',
+            'password' => Hash::make('12345')
         ];
         
-        UserModel::where('username', 'customer-1')->update($data); // tambahkan data ke tabel m_user
+        UserModel::create($data); 
         
-        // coba akses model UserModel
-        $user = UserModel::all(); // ambil semua data dari tabel m_user
+        $user = UserModel::all();
         return view('user', ['data' => $user]);
     }
 }
