17561796477d6dedbe993172a2e62e096ee71346
cindylrs04 <larasaticindy231@gmail.com>
2025-03-15 23:56:37 +0700

    add CRUD barang

--- PWL_POS/app/Models/BarangModel.php
+++ PWL_POS/app/Models/BarangModel.php
@@ -8,9 +8,12 @@ use Illuminate\Database\Eloquent\Relations\BelongsTo;
 class BarangModel extends Model
 {
     protected $table = 'm_barang';
+    protected $primaryKey = 'barang_id';
 
-    public function user(): BelongsTo
+    protected $fillable = ['kategori_id', 'barang_kode', 'barang_nama', 'harga_beli', 'harga_jual'];
+
+    public function kategori(): BelongsTo
     {
-        return $this->belongsTo(UserModel::class);
+        return $this->belongsTo(KategoriModel::class, 'kategori_id', 'kategori_id');
     }
 }
