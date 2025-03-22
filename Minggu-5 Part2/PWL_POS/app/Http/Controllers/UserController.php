204172e79daa38bc1463410ef0cfc5d3eaa18030
cindylrs04 <larasaticindy231@gmail.com>
2025-03-22 21:17:30 +0700

    add praktikum 4 js 5

--- Minggu-5 Part2/PWL_POS/app/Http/Controllers/UserController.php
+++ Minggu-5 Part2/PWL_POS/app/Http/Controllers/UserController.php
@@ -264,30 +264,32 @@ class UserController extends Controller
         ]);
     }
 
-    // Ambil data user dalam bentuk json untuk datatables 
-    public function list(Request $request) 
-    { 
-        $users = UserModel::select('user_id', 'username', 'nama', 'level_id') 
-                    ->with('level'); 
-    
-        return DataTables::of($users) 
-            // menambahkan kolom index / no urut (default nama kolom: DT_RowIndex) 
-            ->addIndexColumn()  
-            ->addColumn('aksi', function ($user) {  // menambahkan kolom aksi 
-                $btn  = '<a href="'.url('/user/' . $user->user_id).'" class="btn btn-info btn
-    sm">Detail</a> '; 
-                $btn .= '<a href="'.url('/user/' . $user->user_id . '/edit').'" class="btn btn
-    warning btn-sm">Edit</a> '; 
-                $btn .= '<form class="d-inline-block" method="POST" action="'. 
-    url('/user/'.$user->user_id).'">' 
-                        . csrf_field() . method_field('DELETE') .  
-                        '<button type="submit" class="btn btn-danger btn-sm" onclick="return 
-    confirm(\'Apakah Anda yakit menghapus data ini?\');">Hapus</button></form>';      
-                return $btn; 
-            }) 
-            ->rawColumns(['aksi']) // memberitahu bahwa kolom aksi adalah html 
-            ->make(true); 
-    } 
+     // Ambil data user dalam bentuk JSON untuk DataTables
+     public function list(Request $request)
+     {
+         $users = UserModel::select('user_id', 'username', 'nama', 'level_id')
+             ->with('level');
+ 
+         // Filter data user berdasarkan level_id
+         if ($request->level_id) {
+             $users->where('level_id', $request->level_id);
+         }
+ 
+         return DataTables::of($users)
+             ->addIndexColumn() // Menambahkan kolom index / nomor urut (default: DT_RowIndex)
+             ->addColumn('aksi', function ($user) { // Menambahkan kolom aksi
+                 $btn  = '<a href="' . url('/user/' . $user->user_id) . '" class="btn btn-info btn-sm">Detail</a> ';
+                 $btn .= '<a href="' . url('/user/' . $user->user_id . '/edit') . '" class="btn btn-warning btn-sm">Edit</a> ';
+                 $btn .= '<form class="d-inline-block" method="POST" action="' . url('/user/' . $user->user_id) . '">'
+                     . csrf_field() . method_field('DELETE') .
+                     '<button type="submit" class="btn btn-danger btn-sm" onclick="return confirm(\'Apakah Anda yakin menghapus data ini?\');">Hapus</button>
+                 </form>';
+                 return $btn;
+             })
+             ->rawColumns(['aksi']) // Memberitahu bahwa kolom aksi mengandung HTML
+             ->make(true);
+     }
+ 
 
      // Menampilkan halaman form tambah user
      public function create()
