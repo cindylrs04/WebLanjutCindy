68538ec0ac7281dbb6bcb9c1602995568521091a
cindylrs04 <larasaticindy231@gmail.com>
2025-03-22 20:52:02 +0700

    add praktikum 3 js 5

--- Minggu-5 Part2/PWL_POS/app/Http/Controllers/UserController.php
+++ Minggu-5 Part2/PWL_POS/app/Http/Controllers/UserController.php
@@ -5,15 +5,17 @@ namespace App\Http\Controllers;
 use App\Models\User;
 use Illuminate\Http\Request;
 use App\Models\userModel;
+use App\Models\LevelModel;
 use App\Models\UserModel as ModelsUserModel;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Hash;
+use Yajra\DataTables\Facades\DataTables;
 
 class UserController extends Controller
 {
     //
-    public function index()
-    {
+    // public function index()
+    // {
         //----------------------------JS 3 ------------------------------
         //     $data = [
         //         'username' => 'customer-1',
@@ -192,10 +194,10 @@ class UserController extends Controller
         // return view('user', ['data' => $user]);
 
         //praktikum 2.7
-        $user = UserModel::with('level')->get();
-        //dd($user);
-        return view('user', ['data' => $user]);
-    }
+    //     $user = UserModel::with('level')->get();
+    //     //dd($user);
+    //     return view('user', ['data' => $user]);
+    // }
 
     //praktikum 2.6
     public function tambah()
@@ -235,4 +237,56 @@ class UserController extends Controller
         $user->delete();
         return redirect('/user');
     }
+
+    // jobsheet 5 pt 2
+
+    //menampilkan halaman awal user
+    public function index()
+    {
+        $breadcrumb = (object) [
+            'title' => 'Daftar User',
+            'list'  => ['Home', 'User']
+        ];
+
+        $page = (object) [
+            'title' => 'Daftar user yang terdaftar dalam sistem'
+        ];
+
+        $activeMenu = 'user'; // set menu yang sedang aktif
+
+        $level = LevelModel::all(); // ambil data level untuk filter level
+
+        return view('user.index', [
+            'breadcrumb' => $breadcrumb,
+            'page'       => $page,
+            'level'      => $level,
+            'activeMenu' => $activeMenu
+        ]);
+    }
+
+    // Ambil data user dalam bentuk json untuk datatables 
+    public function list(Request $request) 
+    { 
+        $users = UserModel::select('user_id', 'username', 'nama', 'level_id') 
+                    ->with('level'); 
+    
+        return DataTables::of($users) 
+            // menambahkan kolom index / no urut (default nama kolom: DT_RowIndex) 
+            ->addIndexColumn()  
+            ->addColumn('aksi', function ($user) {  // menambahkan kolom aksi 
+                $btn  = '<a href="'.url('/user/' . $user->user_id).'" class="btn btn-info btn
+    sm">Detail</a> '; 
+                $btn .= '<a href="'.url('/user/' . $user->user_id . '/edit').'" class="btn btn
+    warning btn-sm">Edit</a> '; 
+                $btn .= '<form class="d-inline-block" method="POST" action="'. 
+    url('/user/'.$user->user_id).'">' 
+                        . csrf_field() . method_field('DELETE') .  
+                        '<button type="submit" class="btn btn-danger btn-sm" onclick="return 
+    confirm(\'Apakah Anda yakit menghapus data ini?\');">Hapus</button></form>';      
+                return $btn; 
+            }) 
+            ->rawColumns(['aksi']) // memberitahu bahwa kolom aksi adalah html 
+            ->make(true); 
+    } 
+
 }
